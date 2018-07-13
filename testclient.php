1f69426a6dbf493dcbfe8bf1de8797edf58064ee
liuguoqing <liuguoqing@chelun.com>
2018-07-13 16:25:48 +0800

    加入swoole spider 代码

--- testclient.php
+++ testclient.php
@@ -1,5 +1,13 @@
 <?php
 $client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+$client->set(array(
+    'open_length_check' => true,
+    'package_max_length' => 2097152,
+    'package_length_type' => 'N',
+    'package_body_offset' => 16,
+    'package_length_offset' => 0,
+));
+
 $client->on("connect", function($cli) {
     $cli->send("hello world\n");
 });
@@ -19,3 +27,4 @@ $client->on("error", function($cli){
 });
 
 $client->connect('127.0.0.1', 19999, 0.5);
+$client->send("hello\n");
