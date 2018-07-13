c76a2afe9645472b7f7aecd77e8ec7a0ff89235e
liuguoqing <liuguoqing@chelun.com>
2018-07-13 15:59:19 +0800

    加入swoole spider 代码

---
+++ testclient.php
@@ -0,0 +1,22 @@
+<?php
+$client = new swoole_client(SWOOLE_ASYNC); //异步非阻塞
+
+$client->on("connect", function($cli) {
+    $cli->send("hello world\n");
+});
+
+$client->on("receive", function($cli, $data) {
+        echo "received: $data\n";
+        sleep(1);
+        $cli->send("hello\n");
+});
+
+$client->on("close", function($cli){
+    echo "closed\n";
+});
+
+$client->on("error", function($cli){
+    exit("error\n");
+});
+
+$client->connect('127.0.0.1', 19999, 0.5);
